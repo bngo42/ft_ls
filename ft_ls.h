96304ad9c5fefe355d53509b3854ef68092e63ea
Bobi NGO <bngo@e1r8p11.42.fr>
2016-09-20 15:36:01 +0200

    Push des fichiers de base

---
+++ ft_ls.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bngo <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/20 15:28:46 by bngo              #+#    #+#             */
+/*   Updated: 2016/09/20 15:33:52 by bngo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+# define FT_LS_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <dirent.h>
+# include <sys/stat.h>
+# include <sys/xattr.h>
+# include <pwd.h>
+# include <time.h>
+
+#endif
