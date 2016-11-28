9a3aff37630bc13e10736e10316c54f14e027ec8
Ludwig VALENTIN <lvalenti@e3r5p9.42.fr>
2016-11-28 14:40:34 +0100

    ls

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bngo <marvin@42.fr>                        +#+  +:+       +#+        */
+/*   By: lvalenti <lvalenti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 11:17:50 by bngo              #+#    #+#             */
-/*   Updated: 2016/11/28 12:31:48 by bngo             ###   ########.fr       */
+/*   Updated: 2016/11/28 14:35:45 by lvalenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,25 @@ char		*ft_check_arg(char **argv)
 	k = 0;
 	while (argv[i])
 	{
-		printf("i= %d\n", i);
+		printf("i = %d\n", i);
 		j = 0;
 		if (ft_strcmp(&argv[i][j], "-"))
 		{
 			j++;
+			printf("j = %d\n", j);
 			while (argv[i][j])
 			{
-				printf("j = %d\n", j);
-				if (ft_strchr("lRart", argv[i][j]))
-					arg[k++] = argv[i][j];
+				if (argv[i][j] == '-')
+				{
+					ft_putendl("Erreur\n");
+					exit (0);
+				}
+				else
+				{
+					printf("j = %d\n", j);
+					if (ft_strchr("lRart", argv[i][j]))
+						arg[k++] = argv[i][j];
+				}
 				j++;
 			}
 		}
@@ -49,11 +58,11 @@ int			main(int argc, char **argv)
 {
 	t_rep *r;
 
+	ft_check_arg(argv);
 	if (!(r = (t_rep*)malloc(sizeof(t_rep))))
 		return (-1);
 	if (!(r->dir = opendir(argv[1])))
 		return (-1);
-	ft_check_arg(argv);
 	while ((r->file = readdir(r->dir)))
 	{
 		ft_putendl(r->file->d_name);
