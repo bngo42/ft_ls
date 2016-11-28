4ab4a087e50aecd3f3f5c572105ee9a1ef054996
Bobi NGO <bngo@e3r6p11.42.fr>
2016-11-28 11:18:38 +0100

    Update

---
+++ libft/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bngo <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/01 18:13:33 by bngo              #+#    #+#             */
+/*   Updated: 2016/10/22 18:15:29 by bngo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 1000
+# include <unistd.h>
+# include "libft.h"
+
+int		get_next_line(int fd, char **line);
+
+#endif
