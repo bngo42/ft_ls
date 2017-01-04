a8005a48f66c094fe68cdde404c7062a7df3c754
Ludwig VALENTIN <lvalenti@e3r6p11.42.fr>
2017-01-04 19:15:24 +0100

    norme shlag

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: lvalenti <lvalenti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/27 09:49:07 by lvalenti          #+#    #+#             */
-/*   Updated: 2017/01/04 19:10:40 by lvalenti         ###   ########.fr       */
+/*   Updated: 2017/01/04 19:14:53 by lvalenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -172,11 +172,28 @@ void		check_type(t_rep *r, t_rep *lst)
 	}
 }
 
+void		assign_opt_2(t_rep *r, t_opt *opt, t_rep *lst)
+{
+	lst->type = r->type;
+	if (opt->t == 1)
+		funct_t(lst);
+	while (lst->prev)
+		lst = lst->prev;
+	if (opt->pr == 1)
+		lst = funct_pr(lst);
+	if (opt->l == 1)
+		funct_l(lst, opt);
+	ft_printlst(lst, opt);
+	ft_putchar('\n');
+	if (opt->gr == 1)
+		funct_gr(lst, opt);
+	free_lst(lst);
+}
+
 void		assign_opt(t_opt *opt, t_rep *r)
 {
 	t_rep			*lst;
 	t_rep			*new;
-	char			*temp;
 
 	ft_memset(opt->len, 0, 8);
 	lst = NULL;
@@ -199,21 +216,7 @@ void		assign_opt(t_opt *opt, t_rep *r)
 		ft_putendl("ls: 3: Not a directory");
 		return ;
 	}
-	lst->type = 0;
-	lst->type = r->type;
-	if (opt->t == 1)
-		funct_t(lst);
-	while (lst->prev)
-		lst = lst->prev;
-	if (opt->pr == 1)
-		lst = funct_pr(lst);
-	if (opt->l == 1)
-		funct_l(lst, opt);
-	ft_printlst(lst, opt);
-	ft_putchar('\n');
-	if (opt->gr == 1)
-		funct_gr(lst, opt);
-	free_lst(lst);
+	assign_opt_2(r, opt, lst);
 }
 
 void		funct_l_2(t_rep *tmp, t_opt *opt)
