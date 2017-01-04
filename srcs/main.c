015f7826f5717162ec7ea3c2653334884fccf9e4
Ludwig VALENTIN <lvalenti@e3r6p11.42.fr>
2017-01-04 19:06:37 +0100

    norme shlag

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: lvalenti <lvalenti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/27 09:49:07 by lvalenti          #+#    #+#             */
-/*   Updated: 2017/01/04 18:59:13 by lvalenti         ###   ########.fr       */
+/*   Updated: 2017/01/04 19:06:22 by lvalenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -250,45 +250,46 @@ void		funct_l(t_rep *r, t_opt *opt)
 	free_lst(lst);
 }
 
+int		read_rep(t_rep *r, t_opt *opt, char *path)
+{
+	struct stat	statfile;
+
+	errno = 0;
+	if (lstat(path, &statfile) < 0)
+	{
+		ft_putstr("ls: ");
+		ft_putstr(path);
+		ft_putstr(": ");
+		perror("");
+		return (-1);
+	}
+	if (S_ISLNK(statfile.st_mode) && opt->l == 1)
+		r->type = 1;
+	else if (!(r->dir = opendir(path)))
+	{
+		if (errno == 13)
+		{
+			perror("ls");
+			return (-1);
+		}
+		else if (errno == 20)
+			r->type = 1;
+	}
+	return (0);
+}
+
 int		read_arg(char *path, t_opt *opt)
 {
 	t_rep		*r;
-	struct stat	statfile;
 
 	if (path != NULL)
 	{
 		r = NULL;
 		if (!(r = (t_rep*)ft_memalloc(sizeof(t_rep))))
 			return (-1);
-		r->name = NULL;
-		r->name2 = NULL;
-		r->argv = NULL;
-		r->next = NULL;
-		r->prev = NULL;
-		r->type = 0;
-		errno = 0;
-		if (lstat(path, &statfile) < 0)
-		{
-			ft_putstr("ls: ");
-			ft_putstr(path);
-			ft_putstr(": ");
-			perror("");
+		if ((read_rep(r, opt, path) < 0))
 			return (-1);
-		}
-		if (S_ISLNK(statfile.st_mode) && opt->l == 1)
-			r->type = 1;
-		else if (!(r->dir = opendir(path)))
-		{
-			if (errno == 13)
-			{
-				perror("ls");
-				return (-1);
-			}
-			else if (errno == 20)
-				r->type = 1;
-		}
 		r->argv = ft_strdup(path);
-		r->flag = 0;
 		assign_opt(opt, r);
 		if (r->type != 1 && (closedir(r->dir) == -1))
 			return (-1);
